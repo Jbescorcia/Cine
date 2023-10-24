ab9dce98833860cfa671f7182b4f767d315bd019
Jesus Barrios <131495042+Jbescorcia@users.noreply.github.com>
2023-10-24 13:58:09 -0500

    Update administrador.php

--- views/administradores/administrador.php
+++ views/administradores/administrador.php
@@ -9,9 +9,23 @@ class administrador
     }
     public function create($nombre, $username, $password, $correo)
     {
-        $sql = "INSERT INTO administrador (nombre, username, password, correo) " .
+        $sqlCheck = "SELECT COUNT(*) FROM administrador WHERE username = '$username'";
+        $resultValido = $this->link->query($sqlCheck);
+        $count = $resultValido->fetch_row()[0];
+
+        if ($count > 0) {
+            echo "<script>alert('Ya existe un registro con el Username proporcionado.');</script>";
+        } else {
+            $sql = "INSERT INTO administrador (nombre, username, password, correo) " .
             "VALUES ('$nombre', '$username', '$password', '$correo')";
-        $result = $this->link->query($sql);
+            $result = $this->link->query($sql);
+
+            if ($result) {
+                echo "<script>alert('Registro insertado con éxito.');</script>";
+            } else {
+                echo "<script>alert('Error al insertar el registro: " . $this->link->error . "');</script>";
+            }
+        }
     }
     public function listar()
     {
