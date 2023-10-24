8438ff422ebcb4b71faa64c1044440f741c6cd88
Jesus Barrios <131495042+Jbescorcia@users.noreply.github.com>
2023-10-24 13:57:35 -0500

    Update conexion.php

--- views/administradores/conexion.php
+++ views/administradores/conexion.php
@@ -3,7 +3,7 @@ class conexion{
 	private $link;
 	
 	public function conectar(){
-		$this->link = new mysqli('cine-do-user-14505311-0.c.db.ondigitalocean.com:25060', 'doadmin', 'AVNS_uaU51AxF7UDKSZEH3_K', 'cine');
+		$this->link = new mysqli('dbcine-do-user-14505311-0.c.db.ondigitalocean.com:25060', 'doadmin', 'AVNS_JXiAmIgJkPoF5o8wprM', 'cine');
 		if ($this->link->connect_errno) {
 			echo "Falló la conexión a MySQL: (" . $this->link->connect_errno . ") " . $this->link->connect_error;
 		}
